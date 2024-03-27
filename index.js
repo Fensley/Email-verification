27e3da9a857ef222676f853aed5ad00ebc9f7914
Fensley <zagalufe@gmail.com>
2024-03-27 12:33:58 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const body = document.querySelector(".container1");
+
+const toggle = document.querySelector(".toggle");
+ 
+
+
+     
+  body.addEventListener("click" , ()=>{
+     toggle.classList.toggle("toggle")
+  })
+
+
